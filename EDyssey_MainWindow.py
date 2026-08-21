122a06eff254916da72f342f045f681cd2324a1b
SalehG <86207813+SalehGholam@users.noreply.github.com>
2026-08-21 16:41:56 +0200

    Bump APP_VERSION after the virtual-detector X/Y fix

--- EDyssey_MainWindow.py
+++ EDyssey_MainWindow.py
@@ -50,7 +50,7 @@ plt.style.use('dark_background')
 # change is made anywhere in the app, so Help > About always reflects how
 # current the running build actually is. Shown directly in the Help menu
 # and repeated in the About dialog.
-APP_VERSION = '2026-08-15 12:00'
+APP_VERSION = '2026-08-15 12:30'
 
 #%% window
 class MainWindow(qtw.QMainWindow):
